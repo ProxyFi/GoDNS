16928f1c62e8992fbe450169edd6cb10b1b8a475
127ewyo3 <226980226+127ewyo3@users.noreply.github.com>
2025-08-17 22:16:08 +0800

    Chore: Create /cmd

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+// cmd/main.go
+package main
+
+import (
+	// Import the godns package to make its functionality available.
+	// This allows us to call the godns.main() function from here.
+	"github.com/ProxyFi/GoDNS"
+)
+
+// main is the entry point for the executable.
+// It directly calls the main function from the imported godns package.
+// This design pattern separates the package logic (godns) from the
+// executable entry point (cmd/main.go).
+func main() {
+	godns.Main()
+}
