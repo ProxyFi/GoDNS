a0eee8bf431254af8f4afc4d149275a2a8da16e7
127ewyo3 <226980226+127ewyo3@users.noreply.github.com>
2025-08-17 20:41:56 +0800

    Update handler.go

--- handler.go
+++ handler.go
@@ -5,7 +5,6 @@ import (
 	"time"
 	
 	"github.com/miekg/dns"
-	
 	"github.com/ProxyFi/GoDNS/features/blocklist"
 )
 
@@ -133,7 +132,7 @@ func (h *GODNSHandler) Do(Net string, w dns.ResponseWriter, req *dns.Msg) {
 	// --- Blocklist check ---
 	domain := UnFqdn(q.Name)
 	if h.blocklist.IsBlocked(domain) {
-		log.Debug("Domain %s is blocked, returning NXDOMAIN", domain)
+		logger.Debug("Domain %s is blocked, returning NXDOMAIN", domain)
 		m := new(dns.Msg)
 		m.SetReply(req)
 		m.SetRcode(req, dns.RcodeNameError) // NXDOMAIN
@@ -183,14 +182,14 @@ func (h *GODNSHandler) Do(Net string, w dns.ResponseWriter, req *dns.Msg) {
 	mesg, err := h.cache.Get(key)
 	if err != nil {
 		if mesg, err = h.negCache.Get(key); err != nil {
-			log.Debug("%s didn't hit cache", Q.String())
+			logger.Debug("%s didn't hit cache", Q.String())
 		} else {
-			log.Debug("%s hit negative cache", Q.String())
+			logger.Debug("%s hit negative cache", Q.String())
 			dns.HandleFailed(w, req)
 			return
 		}
 	} else {
-		log.Debug("%s hit cache", Q.String())
+		logger.Debug("%s hit cache", Q.String())
 		// we need this copy against concurrent modification of Id
 		msg := *mesg
 		msg.Id = req.Id
@@ -201,12 +200,12 @@ func (h *GODNSHandler) Do(Net string, w dns.ResponseWriter, req *dns.Msg) {
 	mesg, err = h.resolver.Lookup(Net, req)
 
 	if err != nil {
-		log.Warn("Resolve query error %s", err)
+		logger.Warn("Resolve query error %s", err)
 		dns.HandleFailed(w, req)
 
 		// cache the failure, too!
 		if err = h.negCache.Set(key, nil); err != nil {
-			log.Warn("Set %s negative cache failed: %v", Q.String(), err)
+			logger.Warn("Set %s negative cache failed: %v", Q.String(), err)
 		}
 		return
 	}
@@ -216,9 +215,9 @@ func (h *GODNSHandler) Do(Net string, w dns.ResponseWriter, req *dns.Msg) {
 	if len(mesg.Answer) > 0 {
 		err = h.cache.Set(key, mesg)
 		if err != nil {
-			log.Warn("Set %s cache failed: %s", Q.String(), err.Error())
+			logger.Warn("Set %s cache failed: %s", Q.String(), err.Error())
 		}
-		log.Debug("Insert %s into cache", Q.String())
+		logger.Debug("Insert %s into cache", Q.String())
 	}
 }
 
